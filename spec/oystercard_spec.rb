18c3c058dc12994d1743a9b7a1f453c403a33569
Ginny <ginnynjon@gmail.com>
2021-10-25 15:41:23 +0100

    wrote failing test that oystercard.balance responds to balance method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,7 @@
+require 'oystercard'
+
 describe Oystercard do
-    
+  it 'has a balance' do
+    expect(subject.balance).to respond_to(balance)  # we want oystercard.balance responds to a balance method
+  end
 end
