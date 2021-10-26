d644629e2af3d878e578365299c1b0279562a4b1
88Andybird <anbird88@gmail.com>
2021-10-26 15:05:29 +0100

    challenge 8 - added touch_in,touch_out,in_journey, methods created

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,14 +1,17 @@
 class Oystercard
 
   attr_reader :balance, :max_balance, :min_balance
+  attr_accessor :in_journey
+
   DEFAULT_BALANCE = 0
   MAX_BALANCE = 90
   MIN_BALANCE = 0
   
-  def initialize(balance = DEFAULT_BALANCE, max_balance = MAX_BALANCE, min_balance = MIN_BALANCE)
+  def initialize(balance = DEFAULT_BALANCE, max_balance = MAX_BALANCE, min_balance = MIN_BALANCE, in_journey = false)
     @balance = balance
     @max_balance = max_balance
     @min_balance = min_balance
+    @in_journey = in_journey
   end
 
   def top_up(value)
@@ -19,6 +22,14 @@ class Oystercard
   def deduct(value)
     fail "this journey would take your balance below £0" if balance-value < @min_balance
     @balance -= value
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
   end
 end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,6 +5,9 @@ describe Oystercard do
     it 'has a default balance' do
       expect(subject.balance).to eq Oystercard::DEFAULT_BALANCE  # we want oystercard.balance to equal the set default balance
     end
+    it "defaults to not in journey" do
+      expect(subject.in_journey).to eq false
+    end
   end
 
   describe '#top_up' do
@@ -31,5 +34,21 @@ describe Oystercard do
     it 'raises an error if balance is less than zero' do #testing edge case of 'minimum balance'
       expect { raise StandardError, "this journey would take your balance below £0"}.to raise_error "this journey would take your balance below £0"
     end
+  end
+  describe '#touch_in' do
+      it 'touches card in' do
+        subject.touch_in
+        expect(subject.in_journey).to eq true
+      end
+
+      describe '#touch_out' do
+        it 'touches card out' do
+          subject.touch_in
+          subject.touch_out
+          expect(subject.in_journey).to eq false
+        end 
+      end
+
+
   end
 end
