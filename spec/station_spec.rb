05160a6b96f676cc805ab8fec956e2b2a6899f9a
Ben Dowsett <bendowsett@gmail.com>
2021-10-27 17:15:29 +0100

    Step 13: test written for station class and passed

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,5 +1,13 @@
 require 'station'
 
 describe Station do
+    describe '#initialize' do
+    it "is initialized with station name & Zone" do
+        station = Station.new("Kings Cross", 1)
+        expect(station.zone).to eq 1
+        expect(station.name).to eq "Kings Cross"
+    end 
+end 
+
     
 end
